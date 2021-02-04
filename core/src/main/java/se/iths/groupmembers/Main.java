520c845c4a9bec9a15da538ce62acb509f9487bd
Tobias <twadseth@icloud.com>
2021-02-04 11:47:04 +0100

    Testing static files

--- core/src/main/java/se/iths/groupmembers/Main.java
+++ core/src/main/java/se/iths/groupmembers/Main.java
@@ -1,8 +1,105 @@
 package se.iths.groupmembers;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+    private static final int PORT = 5050;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Starting server...");
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server started!\nListening on http://localhost:" + PORT);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                executorService.execute(() -> handleConnection(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleConnection(Socket socket) {
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            while (true) {
+                String line = input.readLine();
+                System.out.println(line);
+                if (line.isEmpty()) {
+                    break;
+                }
+            }
+            PrintWriter output = new PrintWriter(socket.getOutputStream());
+
+//            String page = "<html><head><title>Demo page</title></head><body><h1>Hello World</h1></body></html>";
+//
+//            writeHTML(output, page);
+            URL resource = Main.class.getClassLoader().getResource("static"+File.separator+"index.html");
+            File file = new File(resource.toURI());
+            renderFile(socket, file, "text/html; Charset=UTF-8", file.getName());
+            output.flush();
+            socket.close();
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeHTML(PrintWriter writer, String html) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Length: " + html.getBytes().length);
+        writer.println("Content-Type: text/html");
+        writer.println("");
+        writer.println(html);
+    }
+
+    private static void writeHTML(PrintStream printStream, String html) {
+        printStream.println("HTTP/1.1 200 OK");
+        printStream.println("Content-Length: " + html.getBytes().length);
+        printStream.println("Content-Type: text/html");
+        printStream.println();
+        printStream.println(html);
+    }
+
+    private static void renderFile(Socket socket, File file, String fileType, String fileName) {
+        try {
+            PrintStream printStream = new PrintStream(socket.getOutputStream());
+            int length = 0;
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            byte[] bytes = new byte[1024];
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            while ((length = fileInputStream.read(bytes)) != -1) {
+                bos.write(bytes, 0, length);
+            }
+            bos.flush();
+            bos.close();
+            byte[] data1 = bos.toByteArray();
+
+            String dataString = new String(data1, StandardCharsets.UTF_8);
+
+            writeHTML(printStream, dataString);
+
+            printStream.flush();
+            printStream.close();
+
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
