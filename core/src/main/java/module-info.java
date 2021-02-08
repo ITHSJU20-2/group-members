0f10e417bce9d7c88171ea1ba0035f7e2ea42f77
Tobias <twadseth@icloud.com>
2021-02-08 17:55:29 +0100

    Implemented DAO

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,9 +1,6 @@
 import se.iths.groupmembers.spi.Page;
 
 module core {
-//    requires com.fasterxml.classmate;
-    requires dao;
-//    uses UserDAOWithJPAImpl;
     requires spi;
     uses Page;
 }
--- core/src/main/java/se/iths/groupmembers/Main.java
+++ core/src/main/java/se/iths/groupmembers/Main.java
@@ -1,14 +1,10 @@
 package se.iths.groupmembers;
 
-import se.iths.db.UserDAOWithJPAImpl;
-
 public class Main {
 
     private static final int PORT = 5050;
 
     public static void main(String[] args) {
-//        new Server().start(PORT);
-        UserDAOWithJPAImpl dao = new UserDAOWithJPAImpl();
-        System.out.println(dao.getByFirstName("Tobias"));
+        new Server().start(PORT);
     }
 }
