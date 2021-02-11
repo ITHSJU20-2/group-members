cdb2925fccd144bf7be732733307f2c9dc5611c8
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-02-11 12:54:29 +0100

    added deleteuserbylastname method, changed original delete method to deleteuserbyfirstname

--- router/src/main/java/module-info.java
+++ router/src/main/java/module-info.java
@@ -8,7 +8,8 @@ import se.iths.groupmembers.router.get.img.CatIMG;
 import se.iths.groupmembers.router.get.js.PersonJS;
 import se.iths.groupmembers.router.get.pdf.Laboration1PDF;
 import se.iths.groupmembers.router.post.AddUser;
-import se.iths.groupmembers.router.post.DeleteUser;
+import se.iths.groupmembers.router.post.DeleteUserByFirstName;
+import se.iths.groupmembers.router.post.DeleteUserByLastName;
 import se.iths.groupmembers.spi.Page;
 
 module router {
@@ -16,5 +17,5 @@ module router {
     requires dao;
     requires com.google.gson;
     provides Page with Index, Error, Person, NormalizeCSS, MainCSS, CatIMG, Laboration1PDF, PersonJS, GetUsers,
-            AddUser, DeleteUser;
+            AddUser, DeleteUserByFirstName, DeleteUserByLastName;
 }
