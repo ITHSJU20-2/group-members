0174efa2826fe16c4b48e3d9adafeeebf686a4fc
Tobias <twadseth@icloud.com>
2021-02-11 13:52:50 +0100

    Hopefully fixed some stuff

--- router/src/main/resources/static/js/person.js
+++ router/src/main/resources/static/js/person.js
@@ -1,17 +1,15 @@
-(() => {
-    let urlParams = new URLSearchParams(window.location.search);
-    let name = urlParams.get('name')
-    let app = document.getElementById("app");
-    let nameEl = document.querySelector(".name");
+let urlParams = new URLSearchParams(window.location.search);
+let name = urlParams.get('name')
+let app = document.getElementById("app");
+let nameEl = document.querySelector(".name");
 
-    if (name) {
-        document.title = name;
-        nameEl.classList.remove('loading')
-        nameEl.innerText = name;
-    }
+if (name) {
+    document.title = name;
+    nameEl.classList.remove('loading')
+    nameEl.innerText = name;
+}
 
-    app.appendChild(nameEl);
-})()
+app.appendChild(nameEl);
 
 
 
