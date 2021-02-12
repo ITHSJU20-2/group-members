b171a2d73cebd918093d8c8ae50cf333b30a0a8e
Sean <sean.werner@iths.se>
2021-02-12 13:53:51 +0100

    Added module.info

--- router/src/main/java/module-info.java
+++ router/src/main/java/module-info.java
@@ -8,10 +8,7 @@ import se.iths.groupmembers.router.get.img.CatIMG;
 import se.iths.groupmembers.router.get.js.MainJS;
 import se.iths.groupmembers.router.get.js.PersonJS;
 import se.iths.groupmembers.router.get.pdf.Laboration1PDF;
-import se.iths.groupmembers.router.post.AddUser;
-import se.iths.groupmembers.router.post.DeleteUserByFirstName;
-import se.iths.groupmembers.router.post.DeleteUserById;
-import se.iths.groupmembers.router.post.DeleteUserByLastName;
+import se.iths.groupmembers.router.post.*;
 import se.iths.groupmembers.spi.Page;
 
 module router {
@@ -19,5 +16,5 @@ module router {
     requires dao;
     requires com.google.gson;
     provides Page with Index, Error, Person, NormalizeCSS, MainCSS, CatIMG, Laboration1PDF, PersonJS, GetUsers,
-            AddUser, DeleteUserByFirstName, DeleteUserByLastName, DeleteUserById, MainJS;
+            AddUser, DeleteUserByFirstName, DeleteUserByLastName, DeleteUserById, MainJS, UpdateUser;
 }
